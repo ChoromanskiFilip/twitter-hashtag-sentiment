fbf4d11886bca1a73c8469e60fa31c6f659707b1
krolakb <krolakbartlomiej@gmail.com>
2021-01-09 16:44:02 +0100

    Implement aspect based analysis using Azure and absa package

--- test_text_analytics.py
+++ test_text_analytics.py
@@ -1,49 +1,72 @@
-from azure.ai.textanalytics import TextAnalyticsClient
-from azure.core.credentials import AzureKeyCredential
 import requests
 from configparser import ConfigParser
+import aspect_based_sentiment_analysis as absa
 
 config = ConfigParser()
 config.read("config.ini")
-print(config['TextAnalyticsAPI']['endpoint'] + config['TextAnalyticsAPI']['sentiment_url'])
-print(config)
+nlp = absa.load()
 
-# key = config['TextAnalyticsAPI']['key']
-# endpoint = config['TextAnalyticsAPI']['endpoint']
 
-subscription_key = config['TextAnalyticsAPI']['key']
-sentiment_url = config['TextAnalyticsAPI']['endpoint'] + config['TextAnalyticsAPI']['sentiment_url']
-print(subscription_key)
-print(sentiment_url)
+def process(polish_tweet, aspect):
+    english_tweet = translate_to_english(polish_tweet)
+    return analyze_sentiment(english_tweet, aspect)
 
-def analyze_sentiment_myimplement(text):
-    documents = {"documents": [
-        {"id": "1", "language": "pl", "text": text}
+
+def analyze_sentiment(text, aspect):
+    sentiment_azure = analyze_sentiment_azure(text, aspect)
+    if sentiment_azure is None:
+        return analyze_sentiment_absa(text, aspect)
+    else:
+        return sentiment_azure
+
+
+def analyze_sentiment_absa(text, aspect):
+    aspect_sentiment = nlp(text, aspects=[aspect])
+    return {
+        'result': aspect_sentiment.subtasks[aspect].sentiment.name,
+        'positive': aspect_sentiment.subtasks[aspect].scores[2],
+        'negative': aspect_sentiment.subtasks[aspect].scores[1]
+    }
+
+
+def analyze_sentiment_azure(text, aspect):
+    subscription_key = config['TextAnalyticsAPI']['key']
+    sentiment_url = config['TextAnalyticsAPI']['endpoint'] + config['TextAnalyticsAPI']['sentiment_url']
+    body = {"documents": [
+        {"id": "1", "language": "en", "text": text}
     ]}
-    headers = {"Ocp-Apim-Subscription-Key": subscription_key}
-    response = requests.post(sentiment_url, headers=headers, json=documents)
-    sentiments = response.json()
-    print(sentiments)
+    location = config['TextAnalyticsAPI']['location']
+    response = make_request(sentiment_url, body, subscription_key, location)
+    # print(f'Text analytics respone: {response}')
 
-print("Sentyment pozytywny:\n")
-analyze_sentiment_myimplement("#IgaŚwiątek świetna gra. Gratuluje wygranej!")
-print("\nSentyment negatywny:\n")
-analyze_sentiment_myimplement("Słaby występ #IgaŚwiątek")
+    for sentence in response['documents'][0]['sentences']:
+        for found_aspect in sentence['aspects']:
+            if found_aspect['text'] == aspect:
+                return {
+                    'result': found_aspect['sentiment'],
+                    'positive': found_aspect['confidenceScores']['positive'],
+                    'negative': found_aspect['confidenceScores']['negative']
+                }
+    return None
 
-# def authenticate_client():
-#     ta_credential = AzureKeyCredential(key)
-#     text_analytics_client = TextAnalyticsClient(
-#             endpoint=endpoint, 
-#             credential=ta_credential)
-#     return text_analytics_client
 
-# textanalytics_client = authenticate_client()
+def translate_to_english(text):
+    translator_key = config['Translator']['key']
+    translate_url = config['Translator']['endpoint'] + config['Translator']['translate_url']
+    location = config['Translator']['location']
+    body = [{'Text': text}]
+    response = make_request(translate_url, body, translator_key, location)
+    #print(f'Translator respone: {response}')
+    return response[0]['translations'][0]['text']
 
-# def sentiment_analysis(text):
-#     response = textanalytics_client.analyze_sentiment(documents=[text], language='pl')
-#     # print(response)
-#     return response
-#     # sentiment = response.sentiment
 
-# for result in sentiment_analysis("#IgaŚwiątek świetna gra. Gratuluje wygranej!"):
-#     print(result)
+def make_request(url, body, key, location):
+    headers = {"Ocp-Apim-Subscription-Key": key,
+               "Ocp-Apim-Subscription-Region": location,
+               "Content-Type": "application/json"}
+    return requests.post(url, headers=headers, json=body).json()
+
+
+polish_tweet = 'Niestety, bardzo słaby występ #IgaSwiatek, ale trzeba przyznać, że organizacja zawodów była idealna.'
+aspect = '#IgaSwiatek'
+print(process(polish_tweet, aspect))
