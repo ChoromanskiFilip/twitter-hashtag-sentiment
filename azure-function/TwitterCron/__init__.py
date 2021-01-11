3db5a32f2ae41743a4b8975dab2c4f4a594223c7
Filip Choromański <filip.choromanski@wp.pl>
2021-01-11 13:12:21 +0100

    Added function fetch_tweets for historical and daily tweets aggregation.

--- azure-function/TwitterCron/__init__.py
+++ azure-function/TwitterCron/__init__.py
@@ -2,12 +2,12 @@ import datetime
 import logging
 import azure.functions as func
 import requests
-from configparser import ConfigParser
+from configparser import RawConfigParser
 import aspect_based_sentiment_analysis as absa
 import pyodbc
 import os
 
-config = ConfigParser()
+config = RawConfigParser()
 config.read(os.path.join(os.path.dirname(__file__), 'config.ini'))
 server = 'tcp:' + config['Database']['server_url']
 database = config['Database']['database_name']
@@ -175,3 +175,35 @@ def make_request(url, body, key, location):
                "Content-Type": "application/json"}
     return requests.post(url, headers=headers, json=body).json()
 
+
+def fetch_tweets(hashtag, since_id=None, lang='pl', result_type='recent', count=100):
+    """
+    hashtag : string
+    since_id : int  - by default None - fetch all tweets that are available,
+                        should be used for historical tweet aggregation
+                    - if not None - fetch tweets that have id older than provided 
+                        should be used for periodic tweet aggregation - eg. daily
+    """
+    URL = f"{TWITTER_SEARCH_RECENT_URL_V1}"
+    URL += f"?q=%23{hashtag}&lang={lang}&result_type={result_type}&count={count}"
+    URL = URL if not since_id else URL + f"&since_id={since_id}"
+    r = requests.get(URL, headers={'Authorization': f'Bearer {TWITTER_BEARER_TOKEN}'})
+    if r.status_code == 200:
+        response_json = r.json()
+    else:
+        raise Exception("Twitter API is responding with an error: " + json.dumps(r.json()))
+    tweets = []
+    tweets.extend(response_json['statuses'])
+    iters = 100
+    while len(response_json['statuses']) == count and iters > 0:
+        iters -= 1
+        URL = f"{TWITTER_SEARCH_RECENT_URL_V1}{response_json['search_metadata']['next_results']}"
+        URL = URL if not since_id else URL + f"&since_id={since_id}"
+        r = requests.get(URL, headers={'Authorization': f'Bearer {TWITTER_BEARER_TOKEN}'})
+        if r.status_code == 200:
+            response_json = r.json()
+        else:
+            #something went wrong - idk how to handle
+            break
+        tweets.extend(response_json['statuses'])
+    return tweets
--- test_twitter_api.py
+++ test_twitter_api.py
@@ -1,5 +1,6 @@
 import requests
 from configparser import RawConfigParser
+import json 
 
 config = RawConfigParser()
 config.read("config.ini")
@@ -24,10 +25,59 @@ def fetch_tweets(query, version='1', **kwargs):
 def fetch_tweets_v2(hashtag, lang='pl', **kwargs):
     URL = f"{TWITTER_SEARCH_RECENT_URL_V1}?q=%23{hashtag}&lang={lang}"
     URL = URL if not kwargs else URL + '&' + '&'.join([f'{k}={v}' for k, v in kwargs.items()])
-    print(URL)
+    # print(URL)
     r = requests.get(URL, headers={'Authorization': f'Bearer {TWITTER_BEARER_TOKEN}'})
     return r
-    
+
+# def fetch_historical_tweets(hashtag, lang='pl', result_type='recent', count=100):
+#     URL = f"{TWITTER_SEARCH_RECENT_URL_V1}"
+#     URL += f"?q=%23{hashtag}&lang={lang}&result_type={result_type}&count={count}"
+#     r = requests.get(URL, headers={'Authorization': f'Bearer {TWITTER_BEARER_TOKEN}'})
+#     if r.status_code == 200:
+#         response_json = r.json()
+#     else:
+#         raise Exception("Twitter API is responding with an error: " + json.dumps(r.json()))
+#     tweets = []
+#     tweets.extend(response_json['statuses'])
+#     iters = 100
+#     while len(response_json['statuses']) == count and iters > 0:
+#         iters -= 1
+#         URL = f"{TWITTER_SEARCH_RECENT_URL_V1}{response_json['search_metadata']['next_results']}"
+#         r = requests.get(URL, headers={'Authorization': f'Bearer {TWITTER_BEARER_TOKEN}'})
+#         if r.status_code == 200:
+#             response_json = r.json()
+#         else:
+#             #something went wrong - idk how to handle
+#             break
+#         tweets.extend(response_json['statuses'])
+#     return tweets
+
+def fetch_tweets_v3(hashtag, since_id=None, lang='pl', result_type='recent', count=100):
+    URL = f"{TWITTER_SEARCH_RECENT_URL_V1}"
+    URL += f"?q=%23{hashtag}&lang={lang}&result_type={result_type}&count={count}"
+    URL = URL if not since_id else URL + f"&since_id={since_id}"
+    r = requests.get(URL, headers={'Authorization': f'Bearer {TWITTER_BEARER_TOKEN}'})
+    if r.status_code == 200:
+        response_json = r.json()
+    else:
+        raise Exception("Twitter API is responding with an error: " + json.dumps(r.json()))
+    tweets = []
+    tweets.extend(response_json['statuses'])
+    iters = 100
+    while len(response_json['statuses']) == count and iters > 0:
+        iters -= 1
+        URL = f"{TWITTER_SEARCH_RECENT_URL_V1}{response_json['search_metadata']['next_results']}"
+        URL = URL if not since_id else URL + f"&since_id={since_id}"
+        r = requests.get(URL, headers={'Authorization': f'Bearer {TWITTER_BEARER_TOKEN}'})
+        if r.status_code == 200:
+            response_json = r.json()
+        else:
+            #something went wrong - idk how to handle
+            break
+        tweets.extend(response_json['statuses'])
+    return tweets
+        
+
 
 if __name__ == "__main__":
     # tweets = fetch_tweets('IgaŚwiątek', lang='pl', result_type='recent', count=10)
@@ -35,7 +85,12 @@ if __name__ == "__main__":
     # print(tweets.json())
 
     
-    print(fetch_tweets_v2('IgaŚwiątek').json())
+    with open('response_cout2_since-1348018862315466755.json', 'w', encoding='utf-8') as f:
+        # resp = fetch_tweets_v2('IgaŚwiątek', count=100, result_type='recent').json()
+        # json.dump(resp, f, ensure_ascii=False, indent=2)
+        resp = fetch_tweets_v3('IgaŚwiątek', since_id=1348018862315466755 + 1, count=2)
+        json.dump(resp, f, ensure_ascii=False, indent=2)
+        
 
     # print(TWITTER_BEARER_TOKEN)
     # print(TWITTER_SEARCH_RECENT_URL)
